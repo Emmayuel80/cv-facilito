13c52e922e8efd06a98f85d465ee11cc9aeabed0
Silvestre <emmayuel80@gmail.com>
2020-05-04 14:25:55 -0500

    corregido caracter especial

--- PHP/iniciarSesion.php
+++ PHP/iniciarSesion.php
@@ -35,7 +35,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     // Validate credentials
     if(empty($username_err) && empty($password_err)){
         // Prepare a select statement
-        $sql = "SELECT correo, contraseña, imagen, direccion, nombre, ap_materno, ap_paterno FROM usuario WHERE correo = ?";
+        $sql = "SELECT correo, contrasena, imagen, direccion, nombre, ap_materno, ap_paterno FROM usuario WHERE correo = ?";
         
         if($stmt = mysqli_prepare($link, $sql)){
             // Bind variables to the prepared statement as parameters
