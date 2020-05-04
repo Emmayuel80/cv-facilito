cfd0600e3fc0b974f49d4925a602ef0b83fe3b23
Silvestre <emmayuel80@gmail.com>
2020-05-04 14:27:16 -0500

    corregida ruta del perfil

--- PHP/iniciarSesion.php
+++ PHP/iniciarSesion.php
@@ -4,7 +4,7 @@ session_start();
  
 // Check if the user is already logged in, if yes then redirect him to welcome page
 if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-    header("location: profile.php");
+    header("location: perfil.php");
     exit;
 }
  
@@ -67,7 +67,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                             $_SESSION["direccion"] = $direccion;                          
                             
                             // Redirect user to welcome page
-                            header("location: ./profile.php");
+                            header("location: ./perfil.php");
                         } else{
                             // Display an error message if password is not valid
                             $password_err = "The password you entered was not valid.";
