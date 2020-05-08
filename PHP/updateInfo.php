cc5b594d75b06482c5a4f66b111396d5f9f3eafa
Silvestre <emmayuel80@gmail.com>
2020-05-08 17:07:18 -0500

    arreglada consulta

--- PHP/updateInfo.php
+++ PHP/updateInfo.php
@@ -2,7 +2,7 @@
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     $sql = 'UPDATE usuario 
             SET nombre="'. $_POST['nombre'] . '", direccion="' . $_POST['direccion'] . '", fecha_nac="'. $_POST['fecha_nac'] . '",
-                ap_paterno="'. $_POST['ap_paterno'] . '", ap_materno='. $_POST['ap_materno'] . '"
+                ap_paterno="'. $_POST['ap_paterno'] . '", ap_materno="'. $_POST['ap_materno'] . '"
             WHERE correo="'. $_SESSION['correo'] . '"';
     
     $stmt = $link->prepare($sql);
